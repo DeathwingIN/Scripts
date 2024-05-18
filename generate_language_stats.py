36f4768dc541d778171c155c9a4cd84dc1b5ada6
DeathwingIN <imeshnirmal1u@gmail.com>
2024-05-18 15:38:33 +0530

    Update generate_language_stats.py

--- generate_language_stats.py
+++ generate_language_stats.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 GH_USERNAME = os.getenv('GH_USERNAME')
 GH_TOKEN = os.getenv('GH_TOKEN')
+# GH_USERNAME = "DeathwingIN"
+# GH_TOKEN = "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"
 
 def get_repos(username, token=None):
     url = f'https://api.github.com/users/{username}/repos'
@@ -43,22 +45,19 @@ def main():
     fig, ax = plt.subplots(figsize=(8, 6), facecolor='black')  # Set entire image background to black
     bars = ax.barh(languages, percentages, color=colors)
 
-
-    
     for bar, pct in zip(bars, percentages):
         ax.text(bar.get_width() + 1, bar.get_y() + bar.get_height() / 4, f'{pct:.1f}%', va='center', ha='left', color='green')
 
-        ax.set_title('Most Used Languages', color='green')  # Set title color to green
-        ax.invert_yaxis()  # Invert y-axis to display languages from top to bottom
-        ax.set_facecolor('black')  # Set background color inside the chart
-        ax.tick_params(axis='y', colors='green')  # Set font color for y-axis ticks
-        ax.spines['bottom'].set_color('black')  # Remove color for bottom spine
-        ax.spines['left'].set_color('black')  # Remove color for left spine
-        ax.spines['top'].set_color('black')  # Remove color for top spine
-        ax.spines['right'].set_color('black')  # Remove color for right spine
-        ax.xaxis.set_visible(False)  # Remove the x-axis
-        plt.savefig(os.path.join(os.getcwd(), 'language_stats.png'), bbox_inches='tight')  # Save the plot
-
+    ax.set_title('Most Used Languages', color='green')  # Set title color to green
+    ax.invert_yaxis()  # Invert y-axis to display languages from top to bottom
+    ax.set_facecolor('black')  # Set background color inside the chart
+    ax.tick_params(axis='y', colors='green')  # Set font color for y-axis ticks
+    ax.spines['bottom'].set_color('black')  # Remove color for bottom spine
+    ax.spines['left'].set_color('black')  # Remove color for left spine
+    ax.spines['top'].set_color('black')  # Remove color for top spine
+    ax.spines['right'].set_color('black')  # Remove color for right spine
+    ax.xaxis.set_visible(False)  # Remove the x-axis
+    plt.savefig(os.path.join(os.getcwd(), 'language_stats.png'), bbox_inches='tight')  # Save the plot
 
 # Call the main function
 main()
